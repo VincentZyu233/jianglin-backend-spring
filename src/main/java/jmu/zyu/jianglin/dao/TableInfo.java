3ebd1600cb812556601d4dec3b36c044c37e7d8d
ceanCat <al15020@hotmail.com>
2024-06-15 16:58:25 +0100

    fix

--- src/main/java/jmu/zyu/jianglin/dao/TableInfo.java
+++ src/main/java/jmu/zyu/jianglin/dao/TableInfo.java
@@ -17,7 +17,7 @@ public class TableInfo {
 
         String query = "SELECT * FROM information_schema.columns WHERE table_name = ? AND table_schema = 'jianglin' ";
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/test_spring", "root", "diding2014");
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_spring", "root", "diding2014");
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, tableName);
             ResultSet resultSet = preparedStatement.executeQuery();
